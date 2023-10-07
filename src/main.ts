17c5b3af0750585e760f44dd6091f5420ba8829c
Talgat <talgat.s@protonmail.com>
2023-10-07 16:52:48 +0600

    first commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import "reset-css";
+import "@fontsource-variable/fraunces/full.css";
+import "@fontsource-variable/montserrat";
+import "@fontsource-variable/montserrat/wght.css";
+import "@fontsource-variable/montserrat/wght-italic.css";
+import "./main.css";
+import "./button.css";
+import "./image.css";
+import "./style.css";
